7bc3b41988daa353869ff99bee5f45fe069dc0b4
fiona.callahan <fiona.callahan@MU-C1-13.mullen.pvt.k12.co.us>
2023-11-08 14:58:45 -0700

    Uno progress (made Turns the same)

--- Uno/src/Main.java
+++ Uno/src/Main.java
@@ -14,7 +14,10 @@ public class Main
 		static ArrayList<Card> threeDeck = new ArrayList<Card>();
 		static ArrayList<Card> fourDeck = new ArrayList<Card>();
 		static ArrayList<Card> discard = new ArrayList<Card>(); 
+		static int currentPlayerNumber = 1;
+		static boolean clockwise = true;
 		
+		static String playerName;
 		static String playerOne;
 		static String playerTwo;
 		static String playerThree;
@@ -23,6 +26,7 @@ public class Main
 
 		public static void main(String[] args)
 			{
+				
 				
 
 				
@@ -40,13 +44,36 @@ public class Main
 				Deck.shuffle();
 				deal();
 				start();
-				firstTurn();
+				while (oneDeck.size() > 1)
+					{
+						if (currentPlayerNumber == 1 || currentPlayerNumber == 5)
+							{
+								playerName = playerOne;
+								play(oneDeck);
+							}
+						else if (currentPlayerNumber == 2 || currentPlayerNumber == 6)
+							{
+								playerName = playerTwo;
+								play(twoDeck);
+							}
+						else if (currentPlayerNumber == 3 || currentPlayerNumber == -1)
+							{
+								playerName = playerThree;
+								play(threeDeck);
+							}
+						else if (currentPlayerNumber == 4 || currentPlayerNumber == 0)
+							{
+								playerName = playerFour;
+								play(fourDeck);
+							}
+					}
+				
 				
 
 			}
 		public static void deal()
 			{
-				for (int i = 7; i >= 1; i --)
+				for (int i = 6; i >= 0; i --)
 					{
 						oneDeck.add(Deck.deck.get(0));
 						Deck.deck.remove(0);
@@ -65,34 +92,43 @@ public class Main
 				System.out.println("The top card in the deck is " + discard.get(discard.size() - 1).getColor() + " " + discard.get(discard.size() - 1).getIcon());
 			}
 		
-		public static void firstTurn()
+		
+		public static void play(ArrayList <Card> player) 
 			{
 				System.out.println();
-				System.out.println(playerOne + ", its your turn. Here is your deck. Type the number next to the card you would like to play. If you cannot play, and would like to draw, press an 8");
-				for (int i = 0; i <= oneDeck.size() - 1; i ++)
+				System.out.println(playerName + ", its your turn. Here is your deck. Type the number next to the card you would like to play. If you cannot play, and would like to draw, press an 8");
+				for (int i = 0; i <= player.size() - 1; i ++)
 					{
-						System.out.println(i + 1 + ") " + oneDeck.get(i).getColor() + " " +  oneDeck.get(i).getIcon());
+						System.out.println(i + 1 + ") " + player.get(i).getColor() + " " +  player.get(i).getIcon());
 					}
 				int card = userIntInput.nextInt();
 				if (card == 8)
 					{
-						oneDeck.add(Deck.deck.get(0));
+						player.add(Deck.deck.get(0));
 						Deck.deck.remove(0);
-						firstTurn();
+						//play(player);
 					}
-				else if (oneDeck.get(card - 2).getIcon() == "reverse") // REVERSE
+				else if (player.get(card - 1).getIcon() == "reverse") // REVERSE
 					{
-						discard.add(oneDeck.get(card - 2));
+						discard.add(player.get(card - 1));
+						if (clockwise == false) {
+							clockwise = true;
+							currentPlayerNumber ++;
+							}
+						else if (clockwise == true) {
+							clockwise = false;
+							currentPlayerNumber --;
+						}
 					}
-				else if (oneDeck.get(card - 2).getIcon() == "+2") //PLUS TWO
+				else if (player.get(card - 1).getIcon() == "+2") //PLUS TWO
 					{
 						twoDeck.add(Deck.deck.get(0));
 						Deck.deck.remove((0));
 						twoDeck.add(Deck.deck.get(0));
 						Deck.deck.remove(0);
-						discard.add(oneDeck.get(card - 2));
+						discard.add(player.get(card - 1));
 					}
-				else if (oneDeck.get(card - 2).getIcon() == "wild +4") // PLUSFOUR
+				else if (player.get(card - 1).getIcon() == "wild +4") // PLUSFOUR
 					{
 						twoDeck.add(Deck.deck.get(0));
 						Deck.deck.remove((0));
@@ -105,28 +141,59 @@ public class Main
 						System.out.println("What color would you like to change the card to?");
 						String colorPlusFour = userStringInput.nextLine();
 						System.out.println("The color is now " + colorPlusFour + ".");
-						discard.add(oneDeck.get(card - 2));
+						discard.add(player.get(card - 1));
 					}
-				else if (oneDeck.get(card - 2).getIcon() == "wild") // WILD
+				
+				else if (player.get(card - 1).getIcon() == "skip") //SKIP
+					{
+						discard.add(player.get(card - 1));
+						if (clockwise == false)
+							{
+								currentPlayerNumber -= 2;
+							}
+						else if (clockwise == true)
+							{
+								currentPlayerNumber += 2;
+							}
+					
+				else if (player.get(card - 1).getIcon() == "wild") // WILD
 					{
 						System.out.println("What color would you like to change the card to?");
 						String colorPlusFour = userStringInput.nextLine();
 						System.out.println("The color is now " + colorPlusFour + ".");
-						discard.add(oneDeck.get(card - 2));
+						discard.add(player.get(card - 1));
+						if (clockwise == false)
+							{
+								currentPlayerNumber--;
+							}
+						else if (clockwise == true)
+							{
+								currentPlayerNumber++;
+							}
 					}
 				else
 					{
-						discard.add(oneDeck.get(card - 2));
-						oneDeck.remove(card - 2);
+						discard.add(player.get(card - 1));
+						System.out.println(discard.get(0));
+						player.remove(card - 1);
+						if (clockwise == false)
+							{
+								currentPlayerNumber -= 1;
+							}
+						else if (clockwise == true)
+							{
+								currentPlayerNumber += 1;
+							}
 						
 					}
 				
-				discard.add(oneDeck.get(card - 2));
-				oneDeck.remove(card - 2); 
+				//discard.add(oneDeck.get(card - 1));
+				//oneDeck.remove(card - 2); 
 				
 				System.out.println("The new topcard in the deck is " + discard.get(discard.size() - 1).getColor() + " "  + discard.get(discard.size() - 1).getIcon());
 				
 				
 			}
-
+			}
+		
 	}
